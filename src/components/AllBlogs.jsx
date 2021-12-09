6aa8b857ae127c9c95163190bd619ae5b5a1fa5f
stevenspicher <71337603+stevenspicher@users.noreply.github.com>
2021-12-09 00:26:19 -0700

    completed comments

--- src/components/AllBlogs.jsx
+++ src/components/AllBlogs.jsx
@@ -2,14 +2,14 @@ import Comments from './Comments'
 import { Container, Image } from "react-bootstrap";
 import { useSelector } from "react-redux";
 import { selectContent } from "../state/contentSlice";
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 
 const AllBlogs = () => {
     const {index} = useParams()
     const blogInfo = useSelector(selectContent)
    
-   
     return (
+        <>
         <Container className="blog-section-container">
             <h1 className="blog-description">{blogInfo[index].blogDescription}</h1>
             <h3>
@@ -27,8 +27,9 @@ const AllBlogs = () => {
             <Image className="blog-image" src={blogInfo[index].blogPostImg} fluid />
             <h3 className="blog-text">{blogInfo[index].content}</h3>
             {/* //don't need to pass index to comments, the url doesn't change so useParams can get it */}
-            <Comments/>    
         </Container>
+        <Comments/>  
+        </>
     );
 };
 
--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,125 +1,51 @@
 import React, { useState } from "react";
-import { Button, Form, Accordion } from "react-bootstrap";
+import { Container, Button, Form, Accordion, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from 'react-redux';
 import { addComments, selectComments } from '../state/commentSlice';
 import { useParams } from 'react-router-dom'
 
-const Comments = () => {
-  const { index } = useParams();
-  const dispatch = useDispatch();
 
+
+
+const Comments = () => {
+  //grab index from url from parent component link
+  const { index } = useParams();
+
+  const dispatch = useDispatch();
   const commentList = useSelector(selectComments);
   const storeCommentList = () => dispatch(addComments([...commentList, comment]));
 
   const [comment, setComment] = useState({});
-  const [threadNum, setThreadNum] = useState(1);
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    //prevent empty or duplicate comments
+    //prevent empty 
     if (comment.name && comment.comment) {
+      //or duplicate comments
       if (comment !== commentList[commentList.length - 1]) {
 
         // send comment list to redux 
         storeCommentList();
-        setThreadNum(threadNum + 1)
-        
       }
     }
-
-    //reset form and memory
+    //reset form and  component state
     setComment({})
     e.target.reset()
-
-
   }
 
   const updateField = (e) => {
     setComment({
       ...comment,
-      index: index,
-      thread:  threadNum,
       [e.target.name]: e.target.value
     })
-
   }
 
-  // const updateReply = (e) => {
-  //   setReply({
-  //     ...reply,
-  //     index: index,
-  //     thread:  threadNum,
-  //     [e.target.name]: e.target.value
-  //   })
-
-  // }
-
-  //   const onReply = (e) => {
-  //     e.preventDefault();
-
-      //prevent empty or duplicate comments
-      // if (comment.name && comment.comment) {
-      //   if (comment !== commentList[commentList.length - 1]) {
-      //     setReply({
-            
-      //       thread:  thread,
-      //     })
-      //     // send comment list to redux 
-      //     storeCommentList();
-         
-          
-      //   }
-      // }
-  
-      //reset form and memory
-  //     setReply({})
-  //     e.target.reset()
-
-  // }
-
-  // const Replies = (thread) => {
-  //   return (
-
-  //     <div>
-  //     <Accordion >
-  //       <Accordion.Item eventKey="0">
-  //         <Accordion.Header>Reply</Accordion.Header>
-  //         <Accordion.Body>
-  //           <Form onSubmit={onReply}>
-  //             <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-  //               <Form.Control name="name1" placeholder="name" onChange={updateReply} />
-  //             </Form.Group>
-  //             <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-  //               <Form.Control as="textarea" placeholder="comment" name="comment1" rows={3} onChange={updateReply} />
-  //             </Form.Group>
-  //             <Button type='Submit'>Submit</Button>
-  //           </Form>
-  //         </Accordion.Body>
-  //       </Accordion.Item>
-  //     </Accordion>
-  //      {commentList.map((entry, i) => {
-  //       if (entry.thread === thread) {
-  //         return (
-  //           <div key={i}>
-  //             <h2> {entry.name} says:</h2>
-  //             <h3> {entry.comment} </h3>
-              
-  //           </div>
-  //         )
-  //       }
-  //     })}
-    
-  //   </div>
-  //   )
-  // }
-
   return (
-    <div>
-
+    <Container>
       <Accordion >
         <Accordion.Item eventKey="0">
-          <Accordion.Header>Leave a Comment</Accordion.Header>
+          <Accordion.Header className="text-light bg-secondary">Leave a Comment</Accordion.Header>
           <Accordion.Body>
             <Form onSubmit={onSubmit}>
               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -134,24 +60,20 @@ const Comments = () => {
         </Accordion.Item>
       </Accordion>
 
-
-
-
       {commentList.map((entry, i) => {
         if (entry.index === index) {
           return (
-            <div key={i}>
-              <h2> {entry.name} says:</h2>
-              <h3> {entry.comment} </h3>
-              {/* <Replies thread={entry.thread}/> */}
-            </div>
+            <Card key={i} className="mt-2 ">
+              <Card.Header className=" text-light bg-secondary d-flex">{entry.name} says:</Card.Header>
+              <Card.Text className="p-3 d-flex"> {entry.comment} </Card.Text>
+            </Card>
           )
         }
       })}
-    </div>
+    </Container>
   );
 };
 
-
-
 export default Comments;
+
+
